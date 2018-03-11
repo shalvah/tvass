4e5ea0ffbbf5ccdd50880e4fbc41cdd6221f389a
shalvah <diakon.ng@gmail.com>
2018-03-12 00:43:08 +0100

    Add home handler

--- handler.js
+++ handler.js
@@ -2,8 +2,10 @@
 
 const hbs = require('handlebars');
 const fs = require('fs');
+const Pusher = require('pusher');
 
 let visitorCount = 0;
+const updatesChannel = 'visitor-updates';
 
 module.exports.home = (event, context, callback) => {
     let template = fs.readFileSync(__dirname + '/home.hbs', 'utf8');
@@ -14,7 +16,12 @@ module.exports.home = (event, context, callback) => {
         headers: {
             'Content-type': 'text/html'
         },
-        body: template({ visitorCount })
+        body: template({
+            visitorCount,
+            appKey: process.env.PUSHER_APP_KEY,
+            appCluster: process.env.PUSHER_APP_CLUSTER,
+            updatesChannel
+        })
     };
 
     callback(null, response);
